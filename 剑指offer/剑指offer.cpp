c49f3e40ac8378912051204483b45d69b3a80140
SilenceLH <xnjdlhjsj@gmail.com>
2019-05-22 18:17:14 +0800

    ua

--- 剑指offer/剑指offer.cpp
+++ 剑指offer/剑指offer.cpp
@@ -2,10 +2,57 @@
 //
 
 #include <iostream>
+#include "string"
+#include "vector"
+using namespace std;
+/************************************************************************/
+/*                                   1、二维数组中的查找
+在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
+每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，
+判断数组中是否含有该整数。
+*/
+class Solution {
+public:
+/************************************************************************/
+/*                                   1、二维数组中的查找
+在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
+每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，
+判断数组中是否含有该整数。
+*/
+/************************************************************************/
+	bool Find(int tager, vector<vector<int>> array) 
+	{
+		bool res = false;
+		int len1 = array.size();
+		int len2 = array[0].size();
+		for (int i = 0; i  < len1; i++)
+		{
+			for (int j = 0; j < len2; j++)
+			{
+				if (array[i][j] == tager)
+				{
+					res = true;
+				}
+			}
+		}
+		return res;
+	}
+	bool Find1(int tager, vector<vector<int>> array)
+	{
+		int rowCount = array.size();
+		int colCount = array[0].size();
+		int i = rowCount - 1, j = 0;
+
+	}
+
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    cout << "Hello World!\n"; 
+	cout << "asdfa";
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
